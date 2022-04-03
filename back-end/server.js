9c4e412446ec5000a344b900e9e49c309b7fc1b4
ZK34 <70702743+ZK34@users.noreply.github.com>
2022-04-02 19:14:13 -0600

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -88,4 +88,4 @@ app.put('/api/reviews/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3000, () => console.log('Server listening on port 3001!'));
